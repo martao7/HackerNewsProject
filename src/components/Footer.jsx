48e819cf3119bdc34e876c2571182a2a16f39f6f
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-08-29 17:51:59 +0200

    refactor components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="container footer">
+      Click on the Vite and React logos to learn more
+    </footer>
+  );
+}
+
+export default Footer;
