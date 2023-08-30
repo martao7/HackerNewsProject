be7365bfe75c4adece6714ce5aff44cb2244b69e
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-08-30 20:20:36 +0200

    seperate item and itemlist

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Item from "./Item";
 
 function ItemList({ posts }) {
   return (
     <ul className="list-group" id="hits">
-      {posts.length
-        ? posts.map((post, id) => (
-            <li className="list-group-item" key={id}>
-              {post.title}
-              {post.created_at}
-            </li>
+      {posts.length > 0
+        ? posts.map((post, index) => (
+            <Item post={post} key={index} id={index} />
           ))
         : "...loading"}
     </ul>
