466b93a95b4b3e60f1dfe6bd932140f7c51a807a
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-08-30 17:02:24 +0200

    html title, first logo, body bgcolor, added state pages from resultfetch

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,19 @@ import Paginator from "./components/Paginator";
 import ItemList from "./components/ItemList";
 import myData from "./fakeData.json";
 
-document.body.style.backgroundColor = "#011627";
+document.body.style.backgroundColor = "#2ec4b6";
 
 function App() {
   const [hits, setHits] = useState([...myData.hits]);
+  const [pages, setPages] = useState(0);
 
   const handleSearch = async (topic) => {
     const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${topic}`
+      `http://hn.algolia.com/api/v1/search_by_date?query=${topic}`
     );
-
     const searchData = response.data;
     setHits(searchData.hits);
+    setPages(searchData.nbHits);
   };
 
   return (
